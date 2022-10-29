a817a2dd71e6cd25da89da9f5c2a270bc0c18dfb
Julia Malinina <malinina.artwork@gmail.com>
2022-10-29 21:45:14 +0200

    Add decorator and submit EventListener

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,28 @@
+// decorators
+function autoBind(
+  _target: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFunction = originalMethod.bind(this);
+      return boundFunction;
+    }
+  };
+  return adjustedDescriptor;
+};
+
+// ProjectInput Class
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     formElement: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleAmountInputElement: HTMLInputElement;
   
     constructor() {
       this.templateElement = document.getElementById(
@@ -15,7 +36,23 @@ class ProjectInput {
       );
       this.formElement = importedNode.firstElementChild as HTMLFormElement;
       this.formElement.id = 'user-input';
+
+      this.titleInputElement = this.formElement.querySelector('#title') as HTMLInputElement;
+      this.descriptionInputElement = this.formElement.querySelector('#description') as HTMLInputElement;
+      this.peopleAmountInputElement = this.formElement.querySelector('#people') as HTMLInputElement;
+
+      this.configure();
       this.attach();
+    }
+  
+    private submitHandler(event: Event) {
+      event.preventDefault();
+      console.log(this.titleInputElement.value);
+    }
+  
+    @autoBind
+    private configure() {
+      this.formElement.addEventListener('submit', this.submitHandler);
     }
   
     private attach() {
@@ -23,4 +60,4 @@ class ProjectInput {
     }
   }
   
-  const input = new ProjectInput();
+new ProjectInput();
