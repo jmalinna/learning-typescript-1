65fc6bc5e2116fba2b1dbd79c81514aefc9ad054
Julia Malinina <malinina.artwork@gmail.com>
2022-10-29 20:44:42 +0200

    Update styles

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ class ProjectInput {
         true
       );
       this.formElement = importedNode.firstElementChild as HTMLFormElement;
+      this.formElement.id = 'user-input';
       this.attach();
     }
   
