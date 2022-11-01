3895bd080eec6c0b6f128525d728ff9a45c47343
Julia Malinina <malinina.artwork@gmail.com>
2022-11-01 18:40:16 +0100

    Add drag&drop Interfaces and listeners, update styles and HTML

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+// Drag & Drop Interfaces
+interface Draggable {
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+}
+
+interface DragTarget {
+  dragOverHandler(event: DragEvent): void;
+  dropHandler(event: DragEvent): void;
+  dragLeaveHandler(event: DragEvent): void;
+}
+
 // Project Type
 enum ProjectStatus {
   Active,
@@ -131,7 +143,8 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 //ProjectItem Class
-class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
+  implements Draggable {
   private project: Project;
 
   get personsText() {
@@ -150,7 +163,19 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
     this.renderDOMContent();
   }
 
-  configure() {}
+  @autoBind
+  dragStartHandler(event: DragEvent): void {
+    
+  }
+  dragEndHandler(event: DragEvent): void {
+    
+  }
+
+  configure() {
+    this.element.addEventListener('dragstart', this.dragStartHandler);
+    this.element.addEventListener('dragend', this.dragEndHandler);
+  }
+
   renderDOMContent() {
     this.element.querySelector('h2')!.textContent = this.project.title;
     this.element.querySelector('h3')!.textContent = this.personsText + ' assigned';
