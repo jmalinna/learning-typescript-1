8921b5312f9093f3c4454927745cac49d5e91940
Julia Malinina <malinina.artwork@gmail.com>
2022-11-01 12:23:36 +0100

    Add Class State and some generics

--- src/app.ts
+++ src/app.ts
@@ -15,14 +15,22 @@ class Project {
 }
 
 // Project state management
-type Listener = (items: Project[]) => void;
+type Listener<T> = (items: T[]) => void;
 
-class ProjectState {
-  private listeners: any[] = [];
+class State<T> {
+  protected listeners: Listener<T>[] = [];
+
+  addListener(listenerFunction: Listener<T>) {
+    this.listeners.push(listenerFunction);
+  }
+}
+
+class ProjectState extends State<Project> {
   private projects: Project[] = [];
   private static instance: ProjectState;
-  private constructor() {
 
+  private constructor() {
+    super();
   }
 
   static getInstance() {
@@ -30,10 +38,6 @@ class ProjectState {
   
     this.instance = new ProjectState();
     return this.instance;
-  }
-
-  addListener(listenerFunction: Listener) {
-    this.listeners.push(listenerFunction);
   }
 
   addProject(title: string, description: string, peopleAmount: number) {
