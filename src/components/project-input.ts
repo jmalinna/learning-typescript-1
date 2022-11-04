2c215d5cc47dba5e4616fdf8bd0215019ab4037c
Julia Malinina <malinina.artwork@gmail.com>
2022-11-04 23:54:03 +0100

    Move imports from app.ts to components files

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,4 +1,7 @@
 /// <reference path="base-component.ts" />
+/// <reference path="../decorators/autobind.ts" />
+/// <reference path="../utils/validation.ts" />
+/// <reference path="../state/project-state.ts" />
 
 namespace App {
   export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
